624b035fa808983641fdbda59523816abb4de7b1
Lee Sungbo <128334653+ExiledI@users.noreply.github.com>
2023-05-16 15:17:01 +0900

    베이스 코드중 별찍기 기능 부분 수정완료

--- product/src/controller/StarMakerController.java
+++ product/src/controller/StarMakerController.java
@@ -1,8 +1,23 @@
 package controller;
+import model.StarMaker;
+import view.StarMakerView;
+
 
 public class StarMakerController {
     // StarMakerController
+	private StarMakerModel model;
+	private StarMakerView view;
+	public StarMakerController(StarMakerModel model, StarMakerView view) {
+		this.model =model;
+		this.view = view;
+	}
     public void run() {
-
+		StarMakerView starview = new StarMakerView();
+		starview.starMenu();
+		int starNum1 = view.getInputNumber();
+		int starNum2 = view.getInputNumber();
+		model.setStarNum1(starNum1);
+		model.setStarNum2(starNum2);
+		model.StarMaker1();
     }
 }
